06917acde0a050c6252668423fca2e0ca867b700
Timothy Yirenkyi <t.o.yirenkyi@gmail.com>
2022-12-29 16:19:16 +0000

    mizumi program

--- programs/mizumi-program/src/lib.rs
+++ programs/mizumi-program/src/lib.rs
@@ -1,15 +1,409 @@
-use anchor_lang::prelude::*;
+mod constants;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    token,
+    associated_token::AssociatedToken,
+    token::{Mint, Token, TokenAccount}
+};
+declare_id!("GMoknzmqPt9ySN7kinESNqS7zMdw5GSDMRqt9EKUzw55");
 
 #[program]
 pub mod mizumi_program {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // initialize all the accounts for new usdc and usdt pools
+        Ok(())
+    }
+
+    pub fn new_user(ctx: Context<NewUser>) -> Result<()> {
+        if ctx.accounts.admin.key() != constants::ADMIN_PUBKEY {
+            return Err(ProgramError::InvalidInstructionData.into());
+        }
+        // inititalize a user account for new users
+        ctx.accounts.user_account.authority = *ctx.accounts.authority.key;
+
+        Ok(())
+    }
+
+    pub fn first_swap(ctx: Context<FirstSwap>, _new_swap_count: String) -> Result<()> {
+        if ctx.accounts.user_account.swaps_count > 0 {
+            return Err(ProgramError::InvalidInstructionData.into());
+        }
+
+        // initialize the first swap account for a new user
+        ctx.accounts.swap_account.authority = *ctx.accounts.authority.key;
+        ctx.accounts.user_account.swaps_count += 1;
+
+        Ok(())
+    }
+
+    pub fn new_swap(ctx: Context<NewSwap>, _new_swap_count: String) -> Result<()> {
+        if ctx.accounts.admin.key() != constants::ADMIN_PUBKEY {
+            return Err(ProgramError::InvalidInstructionData.into());
+        }
+
+        if !ctx.accounts.current_swap_account.settled {
+            return  Err(ProgramError::InvalidInstructionData.into());
+        }
+        // initialize a swap account
+        ctx.accounts.new_swap_account.authority = *ctx.accounts.authority.key;
+        ctx.accounts.user_account.swaps_count += 1;
+
+        Ok(())
+    }
+
+    pub fn initiate_swap(
+        ctx: Context<Swap>, 
+        token: MizumiStable, 
+        amount: u64, 
+        fiat: MizumiFiat, 
+        tx_kind: TransactionKind,
+        _swap_count: String,
+    ) -> Result<()> {
+        if ctx.accounts.admin.key() != constants::ADMIN_PUBKEY {
+            return Err(ProgramError::InvalidInstructionData.into());
+        }
+        ctx.accounts.swap_account.authority = *ctx.accounts.authority.key;
+        ctx.accounts.swap_account.token = token;
+        ctx.accounts.swap_account.settled = false;
+        ctx.accounts.swap_account.amount_in = amount;
+        ctx.accounts.swap_account.fiat = fiat;
+        ctx.accounts.swap_account.tx_kind = tx_kind;
+        ctx.accounts.swap_account.created_ts = ctx.accounts.clock.unix_timestamp;
+        ctx.accounts.swap_account.bump = *ctx.bumps.get("swap_account").unwrap();
+
+        let usdc_key = &ctx.accounts.usdc.key();
+        let usdt_key = &ctx.accounts.usdt.key();
+
+        let usdc_seeds = &[
+            b"usdc-vault".as_ref(),
+            usdc_key.as_ref(),
+            &[*ctx.bumps.get("usdc_vault").unwrap()],
+        ];
+        let usdc_signer = [&usdc_seeds[..]];
+
+        let usdt_seeds = &[
+            b"usdt-vault".as_ref(),
+            usdt_key.as_ref(),
+            &[*ctx.bumps.get("usdt_vault").unwrap()]
+        ];
+        let usdt_signer = [&usdt_seeds[..]];
+
+        // transfer token from sender -> PDA vault
+        match token {
+            MizumiStable::USDC => {
+                match tx_kind {
+                    TransactionKind::Onramp => {
+                        
+                        let transfer_ctx = CpiContext::new_with_signer(
+                            ctx.accounts.token_program.to_account_info(),
+                            token::Transfer {
+                                from: ctx.accounts.usdc_vault.to_account_info(),
+                                to: ctx.accounts.authority_usdc.to_account_info(),
+                                authority: ctx.accounts.usdc_vault.to_account_info(),
+                            },
+                            &usdc_signer,
+                        );
+                        token::transfer(transfer_ctx, amount)?;
+                    },
+                    TransactionKind::Offramp => {
+                        let transfer_ctx = CpiContext::new(
+                            ctx.accounts.token_program.to_account_info(),
+                            token::Transfer {
+                                from: ctx.accounts.authority_usdc.to_account_info(),
+                                to: ctx.accounts.usdc_vault.to_account_info(),
+                                authority: ctx.accounts.authority.to_account_info()
+                            }
+                        );
+                        token::transfer(transfer_ctx, amount)?;
+                    }
+                }
+                
+            },
+            MizumiStable::USDT => {
+                match tx_kind {
+                    TransactionKind::Onramp => {
+                        let transfer_ctx = CpiContext::new_with_signer(
+                            ctx.accounts.token_program.to_account_info(),
+                            token::Transfer {
+                                from: ctx.accounts.usdt_vault.to_account_info(),
+                                to: ctx.accounts.authority_usdt.to_account_info(),
+                                authority: ctx.accounts.usdt_vault.to_account_info(),
+                            },
+                            &usdt_signer
+                        );
+                        token::transfer(transfer_ctx, amount)?;
+                    },
+                    TransactionKind::Offramp => {
+                        let transfer_ctx = CpiContext::new(
+                            ctx.accounts.token_program.to_account_info(),
+                            token::Transfer {
+                                from: ctx.accounts.authority_usdt.to_account_info(),
+                                to: ctx.accounts.usdt_vault.to_account_info(),
+                                authority: ctx.accounts.authority.to_account_info()
+                            }
+                        );
+                        token::transfer(transfer_ctx, amount)?;
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn complete_swap(
+        ctx: Context<CompleteSwap>,
+        settled: bool,
+        settled_amount: u64,
+        _swap_count: String
+    ) -> Result<()> {
+        if ctx.accounts.admin.key() != constants::ADMIN_PUBKEY {
+            return Err(ProgramError::InvalidInstructionData.into());
+        }
+        ctx.accounts.swap_account.settled = settled;
+        ctx.accounts.swap_account.settled_amount = settled_amount;
+        ctx.accounts.swap_account.settled_ts = ctx.accounts.clock.unix_timestamp;
+        ctx.accounts.user_account.total_swaps_value += settled_amount;
+
         Ok(())
     }
 }
 
+// supported Stablecoins
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq, Debug, Copy)]
+pub enum MizumiStable {
+    USDC,
+    USDT
+}
+
+// supported Fiat currencies
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq, Debug, Copy)]
+pub enum MizumiFiat {
+    GHS,
+    USD,
+}
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq, Debug, Copy)]
+pub enum TransactionKind {
+    Onramp,
+    Offramp,
+}
+
+#[account]
+pub struct SwapAccount {
+    pub authority: Pubkey,
+    pub token: MizumiStable,
+    pub settled: bool,
+    pub amount_in: u64,
+    pub fiat: MizumiFiat,
+    pub tx_kind: TransactionKind,
+    pub settled_amount: u64,
+    pub created_ts: i64,
+    pub settled_ts: i64,
+    pub bump: u8,
+}
+
+
+#[account]
+#[derive(Default)]
+pub struct UserAccount {
+    pub authority: Pubkey,
+    pub swaps_count: u64,
+    pub total_swaps_value: u64, 
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    pub usdc: Account<'info, Mint>,
+    // account to hold usdc
+    #[account(
+        init,
+        payer=payer,
+        seeds=[b"usdc-vault", usdc.key().as_ref()],
+        bump,
+        token::mint = usdc,
+        token::authority = usdc_vault
+    )]
+    pub usdc_vault: Account<'info, TokenAccount>,
+    pub usdt: Account<'info, Mint>,
+    //account to hold usdt
+    #[account(
+        init,
+        payer=payer,
+        seeds=[b"usdt-vault", usdt.key().as_ref()],
+        bump,
+        token::mint = usdt,
+        token::authority = usdt_vault
+    )]
+    pub usdt_vault: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>
+}
+
+#[derive(Accounts)]
+pub struct NewUser<'info> {
+    #[account(signer)]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub admin: AccountInfo<'info>,
+    #[account(signer, mut)]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub authority: AccountInfo<'info>,
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + 32 + 8 + 8,
+        seeds = [b"user-account", authority.key().as_ref()],
+        bump
+    )]
+    pub user_account: Account<'info, UserAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(new_swap_count: String)]
+pub struct FirstSwap<'info> {
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(signer)]
+    pub admin: AccountInfo<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(signer, mut)]
+    pub authority: AccountInfo<'info>,
+    #[account(
+        mut,
+        seeds = [b"user-account", authority.key().as_ref()],
+        bump,
+        has_one = authority
+    )]
+    pub user_account: Account<'info, UserAccount>,
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + 32 + 1 + 1 + 1 + 8 + 1 + 1 + 1 + 1 + 8 + 8 + 8 + 1, 
+        seeds = [b"swap-account", authority.key().as_ref(), new_swap_count.as_ref()], 
+        bump
+    )]
+    pub swap_account: Account<'info, SwapAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(new_swap_count: String)]
+pub struct NewSwap<'info> {
+    #[account(signer)]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub admin: AccountInfo<'info>,
+    #[account(signer, mut)]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub authority: AccountInfo<'info>,
+    #[account(
+        mut,
+        seeds = [b"user-account", authority.key().as_ref()],
+        bump,
+        has_one = authority
+    )]
+    pub user_account: Account<'info, UserAccount>,
+    #[account(
+        init,
+        payer = authority,
+        space = 8 + 32 + 1 + 1 + 1 + 8 + 1 + 1 + 1 + 1 + 8 + 8 + 8 + 1, 
+        seeds = [b"swap-account", authority.key().as_ref(), new_swap_count.as_ref()], 
+        bump
+    )]
+    pub new_swap_account: Account<'info, SwapAccount>,
+    #[account(
+        mut,
+        seeds = [b"swap-account", authority.key().as_ref(), (user_account.swaps_count).to_string().as_ref()],
+        bump,
+    )]
+    pub current_swap_account: Account<'info, SwapAccount>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(token: MizumiStable, amount: u64, fiat: MizumiFiat, tx_kind: TransactionKind, swap_count: String)]
+pub struct Swap<'info> {
+    #[account(signer)]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub admin: AccountInfo<'info>,
+    #[account(signer)]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub authority_usdc: Box<Account<'info, TokenAccount>>,
+    #[account(mut)]
+    pub authority_usdt: Box<Account<'info, TokenAccount>>,
+    #[account(
+        mut,
+        seeds = [b"user-account", authority.key().as_ref()],
+        bump,
+        has_one = authority
+    )]
+    pub user_account: Box<Account<'info, UserAccount>>,
+    #[account(
+        mut, 
+        seeds = [b"swap-account", authority.key().as_ref(), swap_count.as_ref()], 
+        bump,
+        has_one = authority
+    )]
+    pub swap_account: Box<Account<'info, SwapAccount>>,
+    pub usdc: Box<Account<'info, Mint>>,
+    #[account(
+        mut,
+        seeds = [b"usdc-vault", usdc.key().as_ref()],
+        bump,
+        token::mint = usdc,
+        token::authority = usdc_vault
+    )]
+    pub usdc_vault: Box<Account<'info, TokenAccount>>,
+    #[account(
+        mut,
+        seeds = [b"usdt-vault", usdt.key().as_ref()],
+        bump,
+        token::mint = usdt,
+        token::authority = usdt_vault
+    )]
+    pub usdt_vault: Box<Account<'info, TokenAccount>>,
+    pub usdt: Box<Account<'info, Mint>>,
+    pub clock: Sysvar<'info, Clock>,
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+#[instruction(settled: bool, settled_amount: u64, swap_count: String)]
+pub struct CompleteSwap<'info> {
+    #[account(signer)]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub admin: AccountInfo<'info>,
+    #[account(signer, mut)]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub authority: AccountInfo<'info>,
+    #[account(
+        mut,
+        seeds = [b"swap-account", authority.key().as_ref(), swap_count.as_ref()],
+        bump,
+        has_one = authority
+    )]
+    pub swap_account: Account<'info, SwapAccount>,
+    #[account(
+        mut,
+        seeds = [b"user-account", authority.key().as_ref()],
+        bump,
+        has_one = authority
+    )]
+    pub user_account: Account<'info, UserAccount>,
+    pub clock: Sysvar<'info, Clock>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Eq, PartialEq, Clone, Debug)]
+pub struct SwapData {
+    pub token: MizumiStable,
+    pub amount: u64,
+    pub fiat: MizumiFiat,
+    pub tx_kind: TransactionKind
+}
--- tests/mizumi-program.ts
+++ tests/mizumi-program.ts
@@ -1,16 +1,349 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { MizumiProgram } from "../target/types/mizumi_program";
+import {
+  Connection,
+  Keypair, 
+  PublicKey, 
+  SystemProgram, 
+  SYSVAR_CLOCK_PUBKEY, 
+  SYSVAR_RENT_PUBKEY,
+  Transaction, 
+} from "@solana/web3.js";
+import {
+  TOKEN_PROGRAM_ID,
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  createTransferInstruction, 
+  getAssociatedTokenAddressSync
+} from "@solana/spl-token";
+import { expect } from "chai";
+
+enum MizumiStable {
+  USDC = 0,
+  USDT = 1
+}
+
+enum MizumiFiat {
+  GHS = 0,
+  USD = 1,
+}
+
+enum TransactionKind {
+  Onramp = 0,
+  Offramp = 1,
+}
 
 describe("mizumi-program", () => {
+  let provider = anchor.AnchorProvider.env()
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
-
+  anchor.setProvider(provider);
+  
   const program = anchor.workspace.MizumiProgram as Program<MizumiProgram>;
 
+  const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const usdc_pk = new PublicKey("FWA2a9TgjhkTZB1YRofc9QemGn5LbbikbEoAHwbNZBDf")
+    const usdt_pk = new PublicKey("5NJ3dRwgGsEzKo6fwzqizqurjythX7dRALmD3bdUfQK2")
+    const [usdc_vault_pda, sb] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("usdc-vault"),
+        usdc_pk.toBuffer(),
+      ],
+      program.programId
+    ) 
+    const [usdt_vault_pda, vb] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("usdt-vault"),
+        usdt_pk.toBuffer()
+      ],
+      program.programId
+    )
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        usdc: usdc_pk,
+        usdcVault: usdc_vault_pda,
+        usdt: usdt_pk,
+        usdtVault: usdt_vault_pda,
+        payer: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY
+      })
+      .rpc();
+    console.log("initialization transaction signature", tx);
+
+    // get associated token accounts
+    const usdc_associated_acc = getAssociatedTokenAddressSync(
+      usdc_pk, provider.wallet.publicKey, true
+    )
+    const usdt_associated_acc = getAssociatedTokenAddressSync(
+      usdt_pk, provider.wallet.publicKey, true
+    )
+
+    // helper fcn
+    async function print_state() {
+      let balance = await connection.getTokenAccountBalance(usdc_associated_acc)
+      console.log("usdc_vault token amount", balance.value.amount)
+
+      balance = await connection.getTokenAccountBalance(usdt_associated_acc)
+      console.log("usdt_vault token amount", balance.value.amount)
+    }
+
+    // fund vaults
+    const usdcTransferIx = createTransferInstruction(
+      usdc_associated_acc,
+      usdc_vault_pda,
+      provider.wallet.publicKey,
+      10000,
+    )
+    const usdtTransferIx = createTransferInstruction(
+      usdt_associated_acc,
+      usdt_vault_pda,
+      provider.wallet.publicKey,
+      10000,
+    )
+    const blockhash = (await connection.getLatestBlockhash())
+    const fundTx = new Transaction(
+      {
+        feePayer: provider.wallet.publicKey,
+        blockhash: blockhash.blockhash,
+        lastValidBlockHeight: blockhash.lastValidBlockHeight,
+      },
+    )
+    fundTx.instructions = [usdcTransferIx, usdtTransferIx]
+    const signature = await provider.sendAndConfirm(fundTx)
+    console.log("fund vaults transaction", signature)
+
+    await print_state()
   });
-});
+
+  it("creates a new user account", async () => {
+    // NOTE: devnet wallet for testing - CBzqazDdVraumuLRBraVMULDxsha3zkx5FZZW31Lzx6v
+    const admin = Keypair.fromSecretKey(Uint8Array.from([253, 246, 244, 126,  21,  44, 202, 166, 181,  96,
+      105, 195, 127, 161, 149, 102,  48, 175,  77,  97,
+      145,  86, 101,  51, 210, 215, 207, 237, 160, 236,
+      124, 177,  87,  95,   0, 236, 166, 253,  50, 102,
+      207, 248, 123, 105,  43,   9, 251, 191, 182, 203,
+      203, 114,  91, 142, 128, 110, 112,  57, 163, 106,
+      23, 109, 235, 177])
+    );
+    const [user_acc_pda, db] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("user-account"),
+        provider.wallet.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+    const tx = await program.methods
+      .newUser()
+      .accounts({
+        admin: admin.publicKey,
+        userAccount: user_acc_pda,
+        authority: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId
+      })
+      .signers([admin])
+      .rpc()
+    console.log("new user transaction signature", tx)
+  })
+
+  it("creates a new swap account", async() => {
+    // NOTE: devnet wallet for testing - CBzqazDdVraumuLRBraVMULDxsha3zkx5FZZW31Lzx6v
+    const admin = Keypair.fromSecretKey(Uint8Array.from([253, 246, 244, 126,  21,  44, 202, 166, 181,  96,
+      105, 195, 127, 161, 149, 102,  48, 175,  77,  97,
+      145,  86, 101,  51, 210, 215, 207, 237, 160, 236,
+      124, 177,  87,  95,   0, 236, 166, 253,  50, 102,
+      207, 248, 123, 105,  43,   9, 251, 191, 182, 203,
+      203, 114,  91, 142, 128, 110, 112,  57, 163, 106,
+      23, 109, 235, 177])
+    );
+    const [user_acc_pda, db] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("user-account"),
+        provider.wallet.publicKey.toBuffer(),
+      ],
+      program.programId
+    )
+
+    const swaps_count = (await program.account.userAccount.fetch(user_acc_pda)).swapsCount
+    const new_swaps_count = swaps_count.add(new anchor.BN(1))
+
+    const [current_swap_acc_pda, hb] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("swap-account"),
+        provider.wallet.publicKey.toBuffer(),
+        Buffer.from(`${swaps_count.toNumber()}`)
+      ],
+      program.programId
+    )
+    const [swap_acc_pda, sb] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("swap-account"),
+        provider.wallet.publicKey.toBuffer(),
+        Buffer.from(`${new_swaps_count.toNumber()}`),
+      ],
+      program.programId
+    )
+
+    const userAcc = await program.account.userAccount.fetch(user_acc_pda)
+    console.log(userAcc, 'user')
+    
+    const tx = await program.methods
+      .newSwap(`${new_swaps_count.toNumber()}`)
+      .accounts({
+        admin: admin.publicKey,
+        userAccount: user_acc_pda,
+        currentSwapAccount: current_swap_acc_pda,
+        newSwapAccount: swap_acc_pda,
+        authority: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([
+        admin,
+      ])
+      .rpc();
+
+    console.log("new swap transaction signature", tx)
+  })
+
+
+  it("initiates a swap", async () => {
+    // NOTE: devnet tokens for testing
+    const usdc_pk = new PublicKey("FWA2a9TgjhkTZB1YRofc9QemGn5LbbikbEoAHwbNZBDf")
+    const usdt_pk = new PublicKey("5NJ3dRwgGsEzKo6fwzqizqurjythX7dRALmD3bdUfQK2")
+
+    // NOTE: devnet wallet for testing - CBzqazDdVraumuLRBraVMULDxsha3zkx5FZZW31Lzx6v
+    const admin = Keypair.fromSecretKey(Uint8Array.from([253, 246, 244, 126,  21,  44, 202, 166, 181,  96,
+      105, 195, 127, 161, 149, 102,  48, 175,  77,  97,
+      145,  86, 101,  51, 210, 215, 207, 237, 160, 236,
+      124, 177,  87,  95,   0, 236, 166, 253,  50, 102,
+      207, 248, 123, 105,  43,   9, 251, 191, 182, 203,
+      203, 114,  91, 142, 128, 110, 112,  57, 163, 106,
+      23, 109, 235, 177])
+    );
+    const [user_acc_pda, db] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("user-account"),
+        provider.wallet.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+    const swaps_count = (await program.account.userAccount.fetch(user_acc_pda)).swapsCount
+    const [swap_acc_pda, sb] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("swap-account"),
+        provider.wallet.publicKey.toBuffer(),
+        Buffer.from( `${swaps_count.toNumber()}`),
+      ],
+      program.programId
+    )
+    const [usdc_vault_pda, tb] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("usdc-vault"),
+        usdc_pk.toBuffer(),
+      ],
+      program.programId
+    ) 
+    const [usdt_vault_pda, vb] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("usdt-vault"),
+        usdt_pk.toBuffer()
+      ],
+      program.programId
+    )
+
+    // get associated token accounts
+    const usdc_associated_acc = getAssociatedTokenAddressSync(
+      usdc_pk, provider.wallet.publicKey, true
+    )
+    const usdt_associated_acc = getAssociatedTokenAddressSync(
+      usdt_pk, provider.wallet.publicKey, true
+    )
+
+    // helper fcn
+    async function print_state() {
+      const userAcc = await program.account.userAccount.fetch(user_acc_pda)
+      const swapAcc = await program.account.swapAccount.fetch(swap_acc_pda)
+      console.log(swapAcc, 'swap')
+      console.log(userAcc, 'user')
+    }
+
+    const amount = new anchor.BN(100)
+
+    await print_state()
+
+    const tx = await program.methods
+      .initiateSwap({usdc: {}}, amount, {usd: {}}, {onramp: {}}, `${swaps_count.toNumber()}`)
+      .accounts({
+        admin: admin.publicKey,
+        authority: provider.wallet.publicKey,
+        authorityUsdc: usdc_associated_acc,
+        authorityUsdt: usdt_associated_acc,
+        userAccount: user_acc_pda,
+        swapAccount: swap_acc_pda,
+        usdc: usdc_pk,
+        usdcVault: usdc_vault_pda,
+        usdt: usdt_pk,
+        usdtVault: usdt_vault_pda,
+        clock: SYSVAR_CLOCK_PUBKEY,
+      })
+      .signers([admin])
+      .rpc();
+    console.log("initiate swap transaction", tx);
+
+    await print_state()
+  })
+
+  it('completes a swap', async () => {
+    // NOTE: devnet wallet for testing - CBzqazDdVraumuLRBraVMULDxsha3zkx5FZZW31Lzx6v
+    const admin = Keypair.fromSecretKey(Uint8Array.from([253, 246, 244, 126,  21,  44, 202, 166, 181,  96,
+      105, 195, 127, 161, 149, 102,  48, 175,  77,  97,
+      145,  86, 101,  51, 210, 215, 207, 237, 160, 236,
+      124, 177,  87,  95,   0, 236, 166, 253,  50, 102,
+      207, 248, 123, 105,  43,   9, 251, 191, 182, 203,
+      203, 114,  91, 142, 128, 110, 112,  57, 163, 106,
+      23, 109, 235, 177])
+    );
+    const [user_acc_pda, db] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("user-account"),
+        provider.wallet.publicKey.toBuffer(),
+      ],
+      program.programId
+    );
+
+    const swaps_count = (await program.account.userAccount.fetch(user_acc_pda)).swapsCount
+    const [swap_acc_pda, sb] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("swap-account"),
+        provider.wallet.publicKey.toBuffer(),
+        Buffer.from(`${swaps_count.toNumber()}`),
+      ],
+      program.programId
+    )
+
+    const swap_acc = await program.account.swapAccount.fetch(swap_acc_pda)
+    console.log(swap_acc, 'swap')
+
+    const tx = await program.methods
+      .completeSwap(true, new anchor.BN(100), `${swaps_count.toNumber()}`)
+      .accounts({
+        admin: admin.publicKey,
+        authority: provider.wallet.publicKey,
+        swapAccount: swap_acc_pda,
+        userAccount: user_acc_pda,
+        clock: SYSVAR_CLOCK_PUBKEY
+      })
+      .signers([admin])
+      .rpc();
+    
+    const swap_status = (await program.account.swapAccount.fetch(swap_acc_pda)).settled
+    console.log('swap account settled?', swap_status);
+    expect(swap_status).to.be.true;
+  })
+})
